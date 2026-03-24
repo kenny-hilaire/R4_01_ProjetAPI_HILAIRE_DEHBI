fe42c79603c53ac162c9d87fe75bcb1c91b0b73b
Kenny HILAIRE <kenny.hilaire@etu.iut-tlse3.fr>
2026-03-24 12:25:43 +0100

    Test d'essaie

--- API_auth/schema.sql
+++ API_auth/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE IF NOT EXISTS authentification_r401
 
 USE authentification_r401;
 
-CREATE TABLE utilisateurs (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     login VARCHAR(50) NOT NULL UNIQUE,
     mot_de_passe VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL DEFAULT 'user'
 );
-INSERT INTO utilisateurs (login, mot_de_passe, role) 
+INSERT INTO users (login, mot_de_passe, role) 
 VALUES ('admin', 'admin', 'directeur');
-INSERT INTO utilisateurs (login, mot_de_passe, role) 
+INSERT INTO users (login, mot_de_passe, role) 
 VALUES ('player', 'admin', 'joueur');
